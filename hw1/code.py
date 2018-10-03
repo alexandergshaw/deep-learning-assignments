50634153040ade64de4371bc633601f7f65289bf
Alex Shaw <alexandergshaw@gmail.com>
2018-10-02 21:20:17 -0500

    Calculate error weights over iterations.

--- hw1/code.py
+++ hw1/code.py
@@ -6,12 +6,17 @@ def calculateError(w0, w1, x, y):
     return error
     # TODO: REPLACE THIS ERROR FORMULA WITH THE ACTUAL ONE
 
+
+
 print('=================== HOMEWORK 1 ===================')
 file = open('input.txt')
 x = list()
 y = list()
 learningRate = 0.01
-numIterations = 5000
+
+# TODO: CHANGE THIS VALUE BACK TO 5000
+numIterations = 4
+
 minWeightValue = 0
 maxWeightValue = 100
 random.seed(10)
@@ -27,5 +32,14 @@ for row in file:
     x.append(rowEntries[0])
     y.append(rowEntries[0])
 
-for i in list(range(0, numIterations)):
+for epoch in list(range(1, numIterations + 1)):
+    for i in list(range(0, len(x))):
+        xi = float(x[i])
+        yi = float(y[i])
 
+        # TODO: ENSURE THAT THE WEIGHT CALCULATIONS HERE ARE CORRECT
+        error = calculateError(w0, w1, xi, yi)
+        w0 = w0 - (learningRate * error)
+        w1 = w1 - (learningRate * error * xi)
+
+        print(w0, w1)
